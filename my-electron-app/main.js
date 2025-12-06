316cc0b6a26a067b12639862ba9ae6af74cd8d58
Alina <18911926452@163.com>
2025-12-07 00:01:42 +0800

    我的第一个electron程序-渲染器进程到主进程（单向）

---
+++ my-electron-app/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+
+const path = require('node:path')
+
+function handleSetTitle (event, title) {
+  const webContents = event.sender
+  const win = BrowserWindow.fromWebContents(webContents)
+  win.setTitle(title)
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        //   preload: `${__dirname}/preload.js`
+        preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.on('ready', () => {
+  // ipcMain.handle('ping', () => 'pong')
+    ipcMain.on('set-title', handleSetTitle)
+    createWindow()
+    
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ my-electron-app/preload.js
@@ -0,0 +1,26 @@
+// const { contextBridge, ipcRenderer } = require('electron')
+
+// contextBridge.exposeInMainWorld('versions', {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//     // 除函数之外，我们也可以暴露变量
+//   ping: () => ipcRenderer.invoke('ping')
+// })
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector)
+//     if (element) element.innerText = text
+//   }
+
+//   for (const type of ['chrome', 'node', 'electron']) {
+//     replaceText(`${type}-version`, process.versions[type])
+//   }
+// })
+
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  setTitle: (title) => ipcRenderer.send('set-title', title)
+})
